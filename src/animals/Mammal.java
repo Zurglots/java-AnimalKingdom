a5925bce6e0f0b09fe3da6c52e93b16f6322959a
Zurglots <bfeole@gmail.com>
2019-07-10 18:21:41 -0400

    Mammal class created

---
+++ src/animals/Mammal.java
@@ -0,0 +1,16 @@
+package animals;
+
+public class Mammal extends AbstractAnimal
+{
+	private String name;
+	private int yearDiscovered;
+
+	public Mammal(String name, int yearDiscovered)
+	{
+		super(id);
+		this.name = name;
+		this.yearDiscovered = yearDiscovered;
+	}
+
+
+}
