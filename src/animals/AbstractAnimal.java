38c06023525af2120b9ed752deceb6ff14b8b2e5
Zurglots <bfeole@gmail.com>
2019-07-10 18:16:47 -0400

    Remove Animal interface Add AbstractAnimal methods

--- src/animals/AbstractAnimal.java
+++ src/animals/AbstractAnimal.java
@@ -2,5 +2,41 @@ package animals;
 
 public abstract class AbstractAnimal
 {
-	
+	// Fields
+	// Each animal to have, unique id, name, year discovered.
+
+	int id;
+	// String name;
+	// int yearDiscovered;
+
+	// constructors
+
+	public AbstractAnimal()
+	{
+		maxId++;
+		id = maxId;
+	}
+
+	public AbstractAnimal(String name, int yearDiscovered)
+	{
+		this.name = name;
+		this.yearDiscovered = yearDiscovered;
+	}
+
+	// Abstract Methods
+	public abstract String getMove();
+	public abstract String getBreath();
+	public abstract String reproduce();
+
+	// Methods
+
+	public int getId() // Sanity check for proper ID being passed.
+	{
+		return id;
+	}
+
+	public void setId(int id) // May not need this
+	{
+		this.id = id;
+	}
 }
--- src/animals/Animal.java
+++ src/animals/Animal.java
@@ -1,15 +1,14 @@
-package animals;
+// package animals;
 
-public interface animals
+// public interface animals
 
-// All animals consume food equally
-// Each animal to have, unique id, name, year discovered.
-// Methods to return a string stating how that animal implements action.
-// Animals may, move, breath, reproduce.
+// // All animals consume food equally
+// // Methods to return a string stating how that animal implements action.
+// // Animals may: move, breath, reproduce.
 
-{
-	void eat(int i);
-	String move();
-	String breath();
-	String reproduce();
-}
+// {
+// 	void eat(int i);
+// 	String move();
+// 	String breath();
+// 	String reproduce();
+// }
