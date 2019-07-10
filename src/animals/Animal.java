f3aa47e496f3450ba0b5a61a0dd6ccd03007bacb
Zurglots <bfeole@gmail.com>
2019-07-10 17:09:35 -0400

    Animal interface declarations created

--- src/animals/Animal.java
+++ src/animals/Animal.java
@@ -4,7 +4,12 @@ public interface animals
 
 // All animals consume food equally
 // Each animal to have, unique id, name, year discovered.
+// Methods to return a string stating how that animal implements action.
+// Animals may, move, breath, reproduce.
 
 {
-	
+	void eat(int i);
+	String move();
+	String breath();
+	String reproduce();
 }
