ce61081a64322f65ea18664ddf57bd308ef01bd4
Zurglots <bfeole@gmail.com>
2019-07-10 21:29:58 -0400

    Refactor Bird class

--- src/animals/Bird.java
+++ src/animals/Bird.java
@@ -2,14 +2,10 @@ package animals;
 
 public class Bird extends AbstractAnimal
 {
-	private String name;
-	private int yearDiscovered;
 
 	public Bird(String name, int yearDiscovered)
 	{
-		super(id);
-		this.name = name;
-		this.yearDiscovered = yearDiscovered;
+		super(name, yearDiscovered);
 	}
 
 	// Abstracted Methods Overrides
