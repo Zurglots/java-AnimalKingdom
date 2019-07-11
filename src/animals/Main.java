5afadcfc27d1d635f9a8d2f929250e2c70aa4f96
Zurglots <bfeole@gmail.com>
2019-07-10 22:27:43 -0400

    Mammal objects created and added to animalList

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -19,6 +19,27 @@ public class Main
 	}
 	public static void main(String[] args)
 	{
+		ArrayList<AbstractAnimal> animalList = new ArrayList<>(); // create new array list comprised of
+
+		// Construct Mammal Data
+		Mammal panda = new Mammal("Panda", 1869);
+		Mammal zebra = new Mammal("Zebra", 1778);
+		Mammal koala = new Mammal("Koala", 1816);
+		Mammal sloth = new Mammal("Sloth", 1804);
+		Mammal armadillo = new Mammal("Armadillo", 1758);
+		Mammal raccoon = new Mammal("Raccoon", 1758);
+		Mammal bigfoot = new Mammal("Bigfoot!", 2005);
+
+		// Add Mammal Data To animalList
+
+		animalList.add(panda);
+		animalList.add(zebra);
+		animalList.add(koala);
+		animalList.add(sloth);
+		animalList.add(armadillo);
+		animalList.add(raccoon);
+		animalList.add(bigfoot);
+
 
 	}
 }
