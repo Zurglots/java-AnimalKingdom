9c0871a5372bc74df108e9ed93ce2bcb92bd306e
Zurglots <bfeole@gmail.com>
2019-07-10 21:50:23 -0400

    Tester logic implemented in main

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -5,6 +5,18 @@ import java.util*;
 public class Main 
 {
 
+	public static void printAnimals(ArrayList<AbstractAnimal> animals, CheckAnimal tester)
+	{
+		for (AbstractAnimal a : animals)
+			// for each instance of an animal in animals, if the expression sent via printAnimals is true, print the the following for said element. 
+		{
+			if (tester.test(a)) 
+			// test is underfined until we send it the filter request from printAnimals below.
+			{
+				System.out.println("Name: " + a.name + " | " + "Travels by: " + a.getMove() + " | " + "Reproduces by: " + a.reproduce());
+			}
+		}
+	}
 	public static void main(String[] args)
 	{
 
